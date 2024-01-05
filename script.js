f588048e0261d3db8895e6eafe2cb23da1967b6d
Shoib Ansari <shoib.ansari@sap.com>
2024-01-05 06:55:44 +0530

    header set for mobiles

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleMenu=()=>{
+    const menu= document.querySelector(".menu-links");
+    const iconDir = document.querySelector(".three-line-icon");
+    menu.classList.toggle("open")
+    iconDir.classList.toggle("open")
+
+
+}
