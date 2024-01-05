abbb81940b822b8bc45d621638973f6da55606a5
Shoib Ansari <shoib.ansari@sap.com>
2024-01-05 07:46:34 +0530

    trying revert command

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function toggleMenu(){
+function toggleMenu{
     const menu= document.querySelector(".menu-links");
     const iconDir = document.querySelector(".three-line-icon");
     menu.classList.toggle("open")
     iconDir.classList.toggle("open")
-
-
 }
