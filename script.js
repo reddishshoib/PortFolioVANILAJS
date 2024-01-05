c50b15ee5006d061379c94d320a82db41d54616c
Shoib Ansari <shoib.ansari@sap.com>
2024-01-05 07:02:08 +0530

    some js changes

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-const toggleMenu=()=>{
+function toggleMenu{
     const menu= document.querySelector(".menu-links");
     const iconDir = document.querySelector(".three-line-icon");
     menu.classList.toggle("open")
     iconDir.classList.toggle("open")
-
-
 }
