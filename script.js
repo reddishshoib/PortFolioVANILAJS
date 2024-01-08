84c2479e18b5af8fc61eac6cfaca52ba4b16b004
Shoib Ansari <shoib.ansari@sap.com>
2024-01-08 07:40:59 +0530

    responsive navigation done

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ const toggleMenu=()=>{
     menu.classList.toggle("open")
     iconDir.classList.toggle("open")
 
+    var profileSection = document.getElementById("profile");
+    var menuIcon = document.querySelector(".three-line-menu");
 
+    if (profileSection.style.marginTop === "150px") {
+        profileSection.style.transition = "margin-top 0.3s ease"; // Adding transition
+        profileSection.style.marginTop = "0";
+    } else {
+        profileSection.style.transition = "margin-top 0.3s ease"; // Adding transition
+        profileSection.style.marginTop = "150px";
+    }
 }
